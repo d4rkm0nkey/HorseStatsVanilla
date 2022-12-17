74a71130ef30463de4d773ac0f1827fd07e735af
Kichura <68134602+Kichura@users.noreply.github.com>
2022-12-17 21:10:28 +0100

    Set maxmium health to 32.
    Camels have a health value of 32 in vanilla minecraft - and since it shares the same GUI as donkeys, mules, llamas and horses; we need to increase this.

--- src/main/java/monkey/lumpy/horse/stats/vanilla/mixin/HorseScreenMixin.java
+++ src/main/java/monkey/lumpy/horse/stats/vanilla/mixin/HorseScreenMixin.java
@@ -72,7 +72,7 @@ public abstract class HorseScreenMixin extends HandledScreen<HorseScreenHandler>
             if(config.showMaxMin()) {
                 this.textRenderer.draw(matrices, "(4.7-14.2)", 119.0F, 26.0F, config.getNeutralColor().hashCode());
                 this.textRenderer.draw(matrices, "(1-5.3)", 119.0F, 36.0F, config.getNeutralColor().hashCode());
-                this.textRenderer.draw(matrices, "(15-30)", 119.0F, 46.0F, config.getNeutralColor().hashCode());
+                this.textRenderer.draw(matrices, "(15-32)", 119.0F, 46.0F, config.getNeutralColor().hashCode());
             } else {
                 spacer = 10.0F;
             }
