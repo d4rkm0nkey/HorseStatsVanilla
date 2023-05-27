362749d7f95fe1ff7ef63cecb18a73fe73031506
YoungSoulluoS <70120313+YoungSoulluoS@users.noreply.github.com>
2023-05-27 11:01:51 +0500

    Mistake

--- src/main/java/monkey/lumpy/horse/stats/vanilla/util/Converter.java
+++ src/main/java/monkey/lumpy/horse/stats/vanilla/util/Converter.java
@@ -9,13 +9,3 @@ public class Converter {
         return 42.15747598316077 * speed;
     }
 }
-
-/*public class Converter {
-    public static double jumpStrengthToJumpHeight(double strength) {
-        return -0.9078526641492174 * strength * strength * strength + 5.380296913832499 * strength * strength + 0.8696246225981936 * strength - 0.04348078381106464;
-    }
-
-    public static double genericSpeedToBlocPerSec(double speed) {
-        return 42.15747598316077 * speed;
-    }
-}*/
