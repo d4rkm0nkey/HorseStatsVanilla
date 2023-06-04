aec986c67b09544f9a93398aec11cabeec50dc87
D4m0n <Tim.Jeske@web.de>
2023-06-04 12:44:26 +0200

    Remove uncommented code from TooltipDonkey.java

--- src/main/java/monkey/lumpy/horse/stats/vanilla/gui/TooltipDonkey.java
+++ src/main/java/monkey/lumpy/horse/stats/vanilla/gui/TooltipDonkey.java
@@ -45,55 +45,38 @@ public class TooltipDonkey extends LightweightGuiDescription {
         
         WLabel speedSymbol = new WLabel(Text.literal("➟"), speedColor.hashCode());
         WLabel speedLabel = new WLabel(Text.literal(String.valueOf(speed)), speedColor.hashCode());
-        // WLabel speedRange = new WLabel(new LiteralText("(4.8-14.5)"), normalColor);
-
-        // speedSymbol.setVerticalAlignment(VerticalAlignment.CENTER);
 
         speedBox.add(speedSymbol);
         speedBox.add(speedLabel);
-        // speedBox.add(speedRange);
 
         WBox jumpBox = new WBox(Axis.HORIZONTAL);
         
         WLabel jumpSymbol = new WLabel(Text.literal("⇮"), jumpColor.hashCode());
         WLabel jumpLabel = new WLabel(Text.literal(String.valueOf(jump)), jumpColor.hashCode());
-        // WLabel jumpRange = new WLabel(new LiteralText("(1-5.1)"), normalColor);
-        
-        // jumpSymbol.setVerticalAlignment(VerticalAlignment.CENTER);
 
         jumpBox.add(jumpSymbol);
         jumpBox.add(jumpLabel);
-        // jumpBox.add(jumpRange);
 
         WBox healthBox = new WBox(Axis.HORIZONTAL);
         
         WLabel healthSymbol = new WLabel(Text.literal("♥"), hearthColor.hashCode());
         WLabel healthLabel = new WLabel(Text.literal(String.valueOf(health)), hearthColor.hashCode());
-        // WLabel healthRange = new WLabel(new LiteralText("(15-30)"), normalColor);
-        
-        // healthSymbol.setVerticalAlignment(VerticalAlignment.CENTER);
 
         healthBox.add(healthSymbol);
         healthBox.add(healthLabel);
-        // healthBox.add(healthRange);
 
         WBox strengthBox = new WBox(Axis.HORIZONTAL);
 
         WLabel strengthSymbol = new WLabel(Text.literal("▦"), strengthColor.hashCode());
         WLabel strengthLabel = new WLabel(Text.literal(String.valueOf(strength)), strengthColor.hashCode());
-        // WLabel strengthRange = new WLabel(new LiteralText("(1-5.1)"), normalColor);
-
-        // strengthSymbol.setVerticalAlignment(VerticalAlignment.CENTER);
 
         strengthBox.add(strengthSymbol);
         strengthBox.add(strengthLabel);
-        // strengthBox.add(jumpRange);
 
         root.add(speedBox);
         root.add(jumpBox);
         root.add(healthBox);
         root.add(strengthBox);
         root.validate(this);
-
     }
 }
